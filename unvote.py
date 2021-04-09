56dfefaaee16936c732af027942bb6a29871b962
Tim Hütz <tim+github@janke.biz>
2021-04-09 08:01:05 +0200

    chore: slow down the unvote script to ensure to stay in the limits

--- unvote.py
+++ unvote.py
@@ -1,18 +1,23 @@
 if __name__ == '__main__':
     from praw import Reddit
     from prawcore.exceptions import BadRequest
+    from time import sleep
     reddit_api = Reddit('unsave', user_agent='python/unsave-reddit')
     for upvoted_post in reddit_api.user.me().upvoted():
         try:
             submission = reddit_api.submission(id=upvoted_post)
             submission.clear_vote()
             print(f'Unvoted (up) post {upvoted_post}...')
+            sleep(1)
         except BadRequest as e:
             print(f'Failed to unvote (up) post {upvoted_post}. The error was {str(e)}.')
+            sleep(1)
     for downvoted_post in reddit_api.user.me().downvoted():
         try:
             submission = reddit_api.submission(id=downvoted_post)
             submission.clear_vote()
             print(f'Unvoted (down) post {downvoted_post}...')
+            sleep(1)
         except BadRequest as e:
             print(f'Failed to unvote (down) post {downvoted_post}. The error was {str(e)}.')
+            sleep(1)
