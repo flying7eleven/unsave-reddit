708e7d9cc76c0e56ab67bc46766e9870695b5f74
Tim Hütz <tim+github@janke.biz>
2021-04-09 08:08:35 +0200

    chore: slow down the unsave script to ensure to stay in the limits

--- unsave.py
+++ unsave.py
@@ -1,7 +1,9 @@
 if __name__ == '__main__':
     from praw import Reddit
+    from time import sleep
     reddit_api = Reddit('unsave', user_agent='python/unsave-reddit')
     for save in reddit_api.user.me().saved():
         submission = reddit_api.submission(id=save)
         submission.unsave()
         print(f'Unsaved post {save}...')
+        sleep(1)
